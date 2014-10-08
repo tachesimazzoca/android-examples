bf1bb922fc35e0c781e414d3400b45731aab5889
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2014-10-08 17:56:02 +0900

    Call super#onRestart

--- examples/Lifecycle/src/com/github/tachesimazzoca/android/example/lifecycle/MainActivity.java
+++ examples/Lifecycle/src/com/github/tachesimazzoca/android/example/lifecycle/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onRestart() {
-        super.onStart();
+        super.onRestart();
         appendLifecycleLog("onRestart");
     }
 
