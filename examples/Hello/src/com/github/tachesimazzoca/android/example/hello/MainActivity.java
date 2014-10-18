1242c6942508746a5fbd78fd6873dec3ec2c4d46
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2014-10-18 18:22:11 +0900

    Update Hello

--- examples/Hello/src/com/github/tachesimazzoca/android/example/hello/MainActivity.java
+++ examples/Hello/src/com/github/tachesimazzoca/android/example/hello/MainActivity.java
@@ -1,46 +1,33 @@
 package com.github.tachesimazzoca.android.example.hello;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.widget.EditText;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-    public static final String EXTRA_MESSAGE = "example.hello.MainActivity.EXTRA_MESSAGE";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button_send);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                System.out.println("selected action_settings");
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+    private void sendMessage() {
+        Intent intent = new Intent(this, SubActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
         startActivity(intent);
     }
 }
---
+++ examples/Hello/src/com/github/tachesimazzoca/android/example/hello/SubActivity.java
@@ -0,0 +1,31 @@
+package com.github.tachesimazzoca.android.example.hello;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SubActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        // Receives the message via Intent.
+        String message = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+
+        TextView textView = (TextView) findViewById(R.id.text_message);
+        textView.setText(message);
+
+        Button button = (Button) findViewById(R.id.button_done);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
