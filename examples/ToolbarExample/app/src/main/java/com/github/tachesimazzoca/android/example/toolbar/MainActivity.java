9d5b79da3c2f715c61908f2756bc9f1e8db0a7f6
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2014-12-06 16:34:06 +0900

    Add ToolbarExample

---
+++ examples/ToolbarExample/app/src/main/java/com/github/tachesimazzoca/android/example/toolbar/MainActivity.java
@@ -0,0 +1,32 @@
+package com.github.tachesimazzoca.android.example.toolbar;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
